411fe27d5507875252c094fc1301ac17ccde2a64
kfse <sidllub@gmail.com>
2020-09-29 11:42:40 +0200

    started on skeleton of assignment

---
+++ SVM.py
@@ -0,0 +1,14 @@
+import numpy, random, math
+from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+
+class SVM:
+    def __init__(self):
+        1
+        
+    def kernel(self, arg):
+        # Linear kernel K(x,y) = x' * y
+        # Polynomial kernel K(x,y) = (x' * y + r)^p
+        # Radial Basis Functions kernel K(x,y) = e^-(||x-y||^2 / (2*sigma^2))
+        1
+
