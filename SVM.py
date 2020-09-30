9c8aab653350252a82cabe686453a6a6bdc7e5f3
kfse <sidllub@gmail.com>
2020-09-30 19:43:15 +0200

    fml

--- SVM.py
+++ SVM.py
@@ -8,13 +8,14 @@ import matplotlib.pyplot as plt
 
 class SVM:
     def __init__(self, constraint, inputs, targets, arg):
+        self.N = inputs.shape[0]                        # Size of input vector
+
          # For eq 4 in objective()
-        self.t = targets                                # -1, 1 for datapoints
+        self.t = targets.reshape(self.N, 1)             # -1, 1 for datapoints
         self.x = inputs                                 # input vector    
         self.C = constraint                             # 
 
         # Datapoints
-        self.N = inputs.shape[0]                        # Size of input vector
         self.zerofunlist = list()
         self.kern = arg
 
@@ -29,9 +30,9 @@ class SVM:
         # Polynomial kernel K(x,y) = (x' * y + r)^p
         # Radial Basis Functions kernel K(x,y) = e^-(||x-y||^2 / (2*sigma^2))
         if self.kern == "linear":
-            ret = np.dot(x.T, y)
+            ret = np.dot(x,y)
         elif self.kern == "poly":
-            ret = np.power((np.dot(x.T, y) + r), p)
+            ret = np.power((np.dot(x, y) + r), p)
         elif self.kern == "rbf":
             ret = np.exp( -(np.linalg.norm(x-y)**2) / (2*sigma^2) )
         return ret
@@ -51,7 +52,7 @@ class SVM:
     # Implements equation 10
     # Takes vector in as a parameter
     def zerofun(self, alfa):
-        product = np.dot(alfa.T, self.t)
+        product = alfa*self.t
 
         """if np.sum(product)==0:
             return np.sum(product)
@@ -60,6 +61,7 @@ class SVM:
         return np.sum(product)
 
     # Extracts non zero values and adds indices and values into a dictionary
+    # Extracts support vectors referred to as s in lab doc
     def nonZeroExtract(self, alfa):
         ind = np.where(alfa > 1.0e-5)
         dic = {'ind': ind[0],
@@ -69,13 +71,10 @@ class SVM:
         return self.zerofunlist
 
     # Implements equation 7
-    def calculate_b(self, alfa, s, t_s):
+    def calculate_b(self, alfa, s_v, ts_v):
         # sum(alfa_i*t_i*K(s, x) - t_s)
         # alfa_i*t_i
-        temp1 = np.dot(alfa, self.t)
-        # alfa_i*t_i*K(s, x)
-        temp2 = np.dot(temp1, self.kernel(s, self.x)) 
-        b = np.sum(temp2 - t_s)
+        gg = [[a_i*t_i*self.Kernel(s_j, x_i) - ts_j for a_i, t_i, x_i in zip(self.alpha, self.t, self.x)] for s_j, ts_j in zip(s_v, ts_v)]
         return b
 
     def minimize(self):
@@ -97,7 +96,7 @@ class SVM:
         i = 0
         temp1 = np.dot(self.zerofunlist[i]['ind'], self.zerofunlist[i]['targets'])
         temp2 = np.dot(temp1, self.kernel(s, self.zerofunlist[i]['inputs'])) 
-        ind = np.sum(temp2 - self.b)
+        ind = np.sum(temp2 - b)
         return ind
 
 def plot_func(class_a, class_b):
@@ -126,16 +125,31 @@ def main():
     inputs = inputs[permute, :]
     targets = targets[permute]
 
+    # Each datapoint in inputs has an x and y value
     x = inputs[:, 0]
     y = inputs[:, 1]
+
+    s = 2
+    t_s = targets
+    N = targets.shape[0]
     alfa = np.zeros(x.shape[0]).reshape(x.shape[0], 1)
 
     svm = SVM(0.5, inputs, targets, "linear")
+
+    temp1 = alfa*t_s
+    print(inputs[0:2, 0:2])
+    print(np.dot(inputs[0:2, 0:2].T, inputs[0:2, 0:2]))
+    #print("kernel", svm.kernel(inputs[0:1, 0:2], inputs[0:1, 0:2]))
+    
+    
+    #svm.calculate_b(alfa, s, t_s)
+
     for i in range(10):
-        svm.minimize()
+        #svm.minimize()
         #print("zerofun: ", svm.zerofun(alfa))
+        1
     #print("alfa post: ", svm.alpha)
-    svm.nonZeroExtract(targets)
+    #svm.nonZeroExtract(targets)
     # ko = svm.kernel(x, y)
     # o = svm.objective(alfa)
 
